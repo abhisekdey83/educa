9a3f3fded8c24b5a0128e62704f8265a219c2f87
abhisekdey83 <83452588+abhisekdey83@users.noreply.github.com>
2021-05-14 18:56:35 +0530

    Add files via upload
    update courses/api/views.py

--- courses/api/views.py
+++ courses/api/views.py
@@ -9,7 +9,7 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from .serializers import CourseSerializer
-from rest_framework.decorators import detail_route
+#from rest_framework.decorators import detail_route
 from .permissions import IsEnrolled
 from .serializers import CourseWithContentsSerializer
 
@@ -28,18 +28,20 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-    @detail_route(methods=['post'],
-                      authentication_classes=[BasicAuthentication],
-                      permission_classes=[IsAuthenticated])
+    # @detail_route(methods=['post'],
+    #                   authentication_classes=[BasicAuthentication],
+    #                   permission_classes=[IsAuthenticated])
+
     def enroll(self, request, *args, **kwargs):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
 
-    @detail_route(methods=['get'],
-                  serializer_class=CourseWithContentsSerializer,
-                  authentication_classes=[BasicAuthentication],
-                  permission_classes=[IsAuthenticated,
-                                      IsEnrolled])
+    # @detail_route(methods=['get'],
+    #               serializer_class=CourseWithContentsSerializer,
+    #               authentication_classes=[BasicAuthentication],
+    #               permission_classes=[IsAuthenticated,
+    #                                   IsEnrolled])
+    
     def contents(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
